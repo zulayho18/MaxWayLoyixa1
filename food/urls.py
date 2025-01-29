643f292d5907c15a0a102d790e77d31705d9a843
zulayho <xaydarovazulayho5@gmail.com>
2025-01-30 01:40:37 +0500

    food project

---
+++ food/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from food.migrations.views import *
+from .views import *
+urlpatterns = [
+    path('',index,name="index"),
+    path('home_page/',home_page,name="home_page"),
+    path('order_page/',order_page,name="order_page"),
+    path('order/',main_order,name="main_order"),
+]
